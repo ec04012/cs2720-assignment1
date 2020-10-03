094b1fcedc54d67044bfc2a45a4d1ba94898424f
ec04012 <ec04012@uga.edu>
2020-10-03 18:38:49 -0400

    Implemented enqueue dequeue, moved comments

--- QueType.cpp
+++ QueType.cpp
@@ -1,80 +1,96 @@
 #include "QueType.h"
 
+class EmptyQueue {};
+
+class FullQueue {};
+
 template <class ItemType>
 QueType<ItemType>::QueType() {
+    // Class constructor.
     maxQue = 500;
     front = rear = 0;
     items = new ItemType[maxQue];
 }
-// Class constructor.
 
 template <class ItemType>
 QueType<ItemType>::QueType(int max) {
+    // Parameterized class constructor.
     maxQue = max;
     front = rear = 0;
     items = new ItemType[maxQue];
 }
-// Parameterized class constructor.
-template <class ItemType>
-QueType<ItemType>::~QueType() {        
-    delete [] items;
-    front = rear = NULL;
-}
-// Class destructor.
 
 template <class ItemType>
-void QueType<ItemType>::makeEmpty() {    
+QueType<ItemType>::~QueType() {
+    // Class destructor.
+    delete[] items;
+    front = rear = NULL;
+}
+
+template <class ItemType>
+void QueType<ItemType>::makeEmpty() {
+    // Function: Initializes the queue to an empty state.
+    // Post: Queue is empty.
     front = rear = 0;
 }
-// Function: Initializes the queue to an empty state.
-// Post: Queue is empty.
 
 template <class ItemType>
 bool QueType<ItemType>::isEmpty() const {
-    return false;
+    // Function: Determines whether the queue is empty.
+    // Post: Function value = (queue is empty)
+    return front == rear;
 }
-// Function: Determines whether the queue is empty.
-// Post: Function value = (queue is empty)
 
 template <class ItemType>
 bool QueType<ItemType>::isFull() const {
-    return false;
+    // Function: Determines whether the queue is full.
+    // Post: Function value = (queue is full)
+    return (rear + 1) % maxQue == front;
 }
-// Function: Determines whether the queue is full.
-// Post: Function value = (queue is full)
 
 template <class ItemType>
 void QueType<ItemType>::enqueue(ItemType newItem) {
+    // Function: Adds newItem to the rear of the queue.
+    // Post: If (queue is full) FullQueue exception is thrown
+    //       else newItem is at rear of queue.
+    if (isFull) {
+        throw FullQueue();
+    }
+    rear = (rear + 1) % maxQue;
+    items[rear] = newItem;
 }
-// Function: Adds newItem to the rear of the queue.
-// Post: If (queue is full) FullQueue exception is thrown
-//       else newItem is at rear of queue.
 
 template <class ItemType>
 void QueType<ItemType>::dequeue(ItemType& item) {
+    // Function: Removes front item from the queue and returns it
+    // in item.
+    // Post: If (queue is empty) EmptyQueue exception is thrown
+    //       and item is undefined
+    //       else front element has been removed from queue and
+    //       item is a copy of removed element.
+    if (isEmpty()) {
+        throw EmptyQueue();
+    }
+    front = (front + 1) % maxQue;
+    return front;
 }
-// Function: Removes front item from the queue and returns it
-// in item.
-// Post: If (queue is empty) EmptyQueue exception is thrown
-//       and item is undefined
-//       else front element has been removed from queue and
-//       item is a copy of removed element.
 
 template <class ItemType>
 ItemType QueType<ItemType>::peek() {
+    // returns the item at the front of the queue without
+    // removing the item from the queue
+
     ItemType i;
     return i;
 }
-// returns the item at the front of the queue without
-// removing the item from the queue
 
 template <class ItemType>
 int QueType<ItemType>::length() {
+    // Function: returns the number of items in the queue
     return -1;
 }
-// Function: returns the number of items in the queue
 
 template <class ItemType>
 void QueType<ItemType>::printQueue(ofstream&) {
+    // displays QueueItems
 }
-// displays QueueItems
