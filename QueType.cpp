dc7811e59cda919988abbe878d968ff702588146
ec04012 <ec04012@uga.edu>
2020-10-03 16:54:40 -0400

    Implemented Constructors, destructor, makeEmpty

--- QueType.cpp
+++ QueType.cpp
@@ -2,40 +2,49 @@
 
 template <class ItemType>
 QueType<ItemType>::QueType() {
+    maxQue = 500;
+    front = rear = 0;
+    items = new ItemType[maxQue];
 }
 // Class constructor.
 
 template <class ItemType>
 QueType<ItemType>::QueType(int max) {
+    maxQue = max;
+    front = rear = 0;
+    items = new ItemType[maxQue];
 }
 // Parameterized class constructor.
 template <class ItemType>
-QueType<ItemType>::~QueType() {
+QueType<ItemType>::~QueType() {        
+    delete [] items;
+    front = rear = NULL;
 }
 // Class destructor.
 
 template <class ItemType>
-void QueType<ItemType>::makeEmpty() {
+void QueType<ItemType>::makeEmpty() {    
+    front = rear = 0;
 }
 // Function: Initializes the queue to an empty state.
 // Post: Queue is empty.
 
 template <class ItemType>
 bool QueType<ItemType>::isEmpty() const {
-    return false;    
+    return false;
 }
 // Function: Determines whether the queue is empty.
 // Post: Function value = (queue is empty)
 
 template <class ItemType>
 bool QueType<ItemType>::isFull() const {
-  return false;
+    return false;
 }
 // Function: Determines whether the queue is full.
 // Post: Function value = (queue is full)
 
 template <class ItemType>
-void QueType<ItemType>::enqueue(ItemType newItem) {  
+void QueType<ItemType>::enqueue(ItemType newItem) {
 }
 // Function: Adds newItem to the rear of the queue.
 // Post: If (queue is full) FullQueue exception is thrown
@@ -53,15 +62,15 @@ void QueType<ItemType>::dequeue(ItemType& item) {
 
 template <class ItemType>
 ItemType QueType<ItemType>::peek() {
-  ItemType i;
-  return i;
+    ItemType i;
+    return i;
 }
 // returns the item at the front of the queue without
 // removing the item from the queue
 
 template <class ItemType>
 int QueType<ItemType>::length() {
-  return -1;
+    return -1;
 }
 // Function: returns the number of items in the queue
 
